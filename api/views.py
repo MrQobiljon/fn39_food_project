8775a23cdc802ba4061018b7f18488db660e0cc8
Qobiljon <madrahimovq@gmail.com>
2026-06-01 10:05:24 +0500

    4-kun

--- api/views.py
+++ api/views.py
@@ -1,27 +1,56 @@
 from django.forms.models import model_to_dict
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.request import Request
 from rest_framework.response import Response
+from rest_framework import status
+from unicodedata import category
 
 from .models import Category, Food
+from .serializers import CategorySerializer
 
 
 class CategoryAPIView(APIView):
     def get(self, request: Request, pk: int = None):
         if not pk:
             categories = Category.objects.all()
-            category_list = []
-            for category in categories:
-                category_list.append(
-                    {
-                        'id': category.pk,
-                        'name': category.name
-                    }
-                )
-            return Response(category_list)
+            return Response(CategorySerializer(categories, many=True).data)
         else:
-            category = Category.objects.get(pk=pk)
-            return Response(model_to_dict(category))
+            category = get_object_or_404(Category, pk=pk)
+            return Response(CategorySerializer(category).data)
+
+    def post(self, request: Request, pk = None):
+
+        if pk:
+            return Response({"message": "Method POST not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        serializer = CategorySerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        category = Category.objects.create(**serializer.validated_data)
+        return Response(CategorySerializer(category).data)
+
+    def put(self, request: Request, pk: int = None):
+        if pk:
+            category = get_object_or_404(Category, pk=pk)
+            serializer = CategorySerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+
+            category.name = serializer.validated_data.get("name", category.name)
+            category.save()
+
+            return Response(CategorySerializer(category).data)
+        else:
+            return Response({"message": "Method PUT not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def delete(self, request: Request, pk: int = None):
+        if not pk:
+            return Response({"message": "Method DELETE not allowed"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        else:
+            category = get_object_or_404(Category, pk=pk)
+            category.delete()
+            return Response({"message": "Category deleted successful"}, status=status.HTTP_204_NO_CONTENT)
+
+
 
 
 class FoodAPIView(APIView):
